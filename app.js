5eb39d47c8df4ee4b2c17831668cacccfd5a09c5
Loki <zpk.loki@gmail.com>
2019-11-08 15:46:20 +0300

    js post put deleted

--- app.js
+++ app.js
@@ -55,11 +55,11 @@ const http = new easyHTTP();
 
 //Delete Post
 http.delete('https://jsonplaceholder.typicode.com/posts/1', 
-function(err, posts) {
+function(err, response) {
   if(err){
     console.log(err)
   }else {
-    console.log(posts);
+    console.log(response);
   }
   
 }); 
